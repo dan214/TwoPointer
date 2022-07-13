197ce6c44a94eed75042e969982548ba91123466
Daniel Irungu <irungu214@gmail.com>
2022-07-13 20:31:34 +0300

    initial commit

---
+++ TwoPointer/TwoPointer/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+
+namespace TwoPointer // Note: actual namespace depends on the project name.
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] result = TestMethod(new int[] {1,2,3,4,6}, 6);
+
+            for(int i = 0; i < result.Length; i++)
+            {
+                Console.WriteLine(result[i]);
+            }
+        }
+
+        private static int[] TestMethod(int[] intArr, int target)
+        {
+            int endPoint = intArr.Length - 1;
+            int startPoint = 0;
+
+            while(intArr[startPoint] + intArr[endPoint] != target)
+            {
+                if (intArr[startPoint] + intArr[endPoint] > target)
+                {
+                    --endPoint;
+                }
+                else
+                {
+                    ++startPoint;
+                }
+            }
+
+            return new int[] { startPoint, endPoint };
+
+        }
+    }
+}
